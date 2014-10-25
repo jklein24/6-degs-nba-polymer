029341f7859d2f74d50b8113e28041b83d18fff9
Jeremy Klein <jklein24@gmail.com>
2014-10-24 23:41:34 -0700

    Trying to get typeahead to work...

--- six-degs-app.js
+++ six-degs-app.js
@@ -11,4 +11,55 @@ Polymer('six-degs-app', {
     this.$.ajax.body = "source="+this.getSourceValue()+"&dest="+this.getDestValue();
     this.$.ajax.go();
   },
+
+  substringMatcher: function(strs) {
+    return function findMatches(q, cb) {
+      var matches, substrRegex;
+
+      // an array that will be populated with substring matches
+      matches = [];
+
+      // regex used to determine if a string contains the substring `q`
+      substrRegex = new RegExp(q, 'i');
+
+      // iterate through the pool of strings and for any string that
+      // contains the substring `q`, add it to the `matches` array
+      $.each(strs, function(i, str) {
+        if (substrRegex.test(str)) {
+          // the typeahead jQuery plugin expects suggestions to a
+          // JavaScript object, refer to typeahead docs for more info
+          matches.push({ value: str });
+        }
+      });
+
+      cb(matches);
+    };
+  },
+
+  ready: function() {
+    $.getJSON('player-names.json')
+        .done(function(data) {
+          $('#source').typeahead({
+            hint: true,
+            highlight: true,
+            minLength: 1
+          },
+          {
+            name: 'states',
+            displayKey: 'value',
+            source: this.substringMatcher(data)
+          });
+
+          $('#dest').typeahead({
+            hint: true,
+            highlight: true,
+            minLength: 1
+          },
+          {
+            name: 'states',
+            displayKey: 'value',
+            source: this.substringMatcher(data)
+          });
+        }.bind(this));
+  }
 });
